55217a1c755dbee0253ec2332f38aafa26291151
khaled <khaledgamal1345@gmail.com>
2023-08-04 21:46:37 +0300

    added canonical url

--- blog/views.py
+++ blog/views.py
@@ -7,14 +7,18 @@ def post_list(request):
     posts= Post.published.all()
     return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     #the first id is from database
     #     post= Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404('No Post found.')
     
-    #the first id is from database
-    post = get_object_or_404(Post, id=id, status= Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                            status= Post.Status.PUBLISHED,
+                            slug=post, #slug of the post = post that in the request url in urls.py
+                            publish__year= year,
+                            publish__month= month,
+                            publish__day= day)
     
     return render(request, 'blog/post/detail.html', {'post':post})
