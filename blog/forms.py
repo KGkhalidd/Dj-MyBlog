36e8f37ec22ed2d1a2a17bb127f07fe2720a2899
khaled <khaledgamal1345@gmail.com>
2023-09-10 01:27:02 +0300

    added search field for users to search in posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Comment 
 
+class SearchForm(forms.Form):
+    query = forms.CharField()
 
 class CommentForm(forms.ModelForm):
     
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,34 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage , PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm , SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 # from django.http import Http404
 # Create your views here.
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET :
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search= SearchVector('title', 'body'),
+            ).filter(search=query)
+
+    context= {
+        'form':form,
+        'query':query,
+        'results': results
+    }
+    return render(request, 'blog/post/search.html', context)
 
 
 def post_share(request, post_id):
